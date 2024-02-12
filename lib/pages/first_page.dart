334331cd0b631b25e078c70a13b84dcfb24ebcbc
single-byte <joharkhan1999@gmail.com>
2024-02-12 21:29:15 +0500

    Refactor drawer in FirstPage

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -10,23 +10,56 @@ class FirstPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("First Page"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Got to Second Page"),
-          onPressed: () {
-            // navigate to second page
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(
-            //     builder: (context) => SecondPage(),
-            //   ),
-            // );
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple.shade200,
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(
+                Icons.favorite,
+                size: 50,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                // close/pop drawer first
+                Navigator.pop(context);
 
-            // 2nd way to navigate
-            Navigator.pushNamed(context, "/secondpage");
-          },
+                Navigator.pushNamed(context, "/firstpage");
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {
+                // close/pop drawer first
+                Navigator.pop(context);
+
+                Navigator.pushNamed(context, "/secondpage");
+              },
+            )
+          ],
         ),
       ),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: Text("Got to Second Page"),
+      //     onPressed: () {
+      //       // navigate to second page
+      //       // Navigator.push(
+      //       //   context,
+      //       //   MaterialPageRoute(
+      //       //     builder: (context) => SecondPage(),
+      //       //   ),
+      //       // );
+
+      //       // 2nd way to navigate
+      //       Navigator.pushNamed(context, "/secondpage");
+      //     },
+      //   ),
+      // ),
     );
   }
 }
